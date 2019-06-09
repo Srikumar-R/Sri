08989c0448194fb4865fb5754ef1ff66522ec5bf
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-09 16:47:14 +0530

    Update 25pl.py

--- 25pl.py
+++ 25pl.py
@@ -1,23 +1,25 @@
+def swap(f,k):
+    t=f
+    f=k
+    k=t
+    return(f,k)
 a=int(input())
 b=input().split()
-c=0
-d=[]
-e=[]
-f=''
-for i in b:
-  for y in i:
-    c+=ord(y)
-  d.append(c)
-  c=0
-i=0
-while i<len(b):
-  e.append(b[d.index(min(d,key=int))])
-  b.pop(d.index(min(d,key=int)))
-  d.pop(d.index(min(d,key=int)))
-  i+=1
-for i in range(len(e)):
-  f+=e[i]+" "
-if b!=[]:
-  print(f+b[0])
-else:
-  print(f)
+c=[]
+d=''
+b.sort(key=len)
+for i in range(a-1):
+    print(b[i])
+    if len(b[i])==len(b[i+1]):
+        for j in range(len(b[i])):
+            if b[i][j]>b[i+1][j]:
+                x,y=swap(b[i],b[i+1])
+                if x not in c:
+                    c.append(x)
+                if y not in c:
+                    c.append(y)
+    elif b[i] not in c:
+        c.append(b[i])
+if b[-1] not in c:
+    c.append(b[-1])
+print(c)
