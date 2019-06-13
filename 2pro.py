50ac73965fa8dc56d4786e083b924ad087e26542
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-13 14:47:56 +0530

    Update 2pro.py

--- 2pro.py
+++ 2pro.py
@@ -1,9 +1,9 @@
-u,v=input().split()
+x,v=input().split()
 c=0
 d=-1
 for i in range(int(v)):
     if i%2==0:
-        u=u[c+1:]
+        x=x[c+1:]
     if i%2==1:
-        u=u[:d]
-print(u)
+        x=x[:d]
+print(x)
