6bd942a90f73504e48a1882ac68ad9fd80267a4a
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-14 10:12:30 +0530

    Create 108pl.py

---
+++ 108pl.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=input().split()
+d=0
+e=[]
+f=''
+for i in range(a):
+    c=b[:i+1]
+    for y in range(len(c)):
+        d+=int(c[y])
+    e.append(d)
+    d=0
+for i in range(len(e)-1):
+    f+=str(e[i])+" "
+print(f+str(e[-1]))
