4abdc73daa92965d042e5ddd7bcf3248e2d260f7
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-07-16 09:47:29 +0530

    Update 3p.py

--- 3p.py
+++ 3p.py
@@ -1,10 +1,15 @@
-a,b=input().split()
-c=0
-if a==b:
+def insert(a,b):
     c=0
-else:
     for i in range(len(a)):
         if a[i]==b[i]:
             c+=1
     c=len(b)-c
+    return(c)
+a,b=input().split()
+if a==b:
+    c=0
+elif len(a)<=len(b):
+    c=insert(a,b)
+elif len(a)>len(b):
+    c=insert(b,a)
 print(c)
